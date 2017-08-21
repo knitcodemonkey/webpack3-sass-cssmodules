1abc2f5665ed4d38608c1ddab4502d724441a837
Jennifer Luker <jluker@deseretdigital.com>
2017-08-21 14:03:35 -0600

    fixing missing css reference

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,9 @@ const Home = () => (
     <article>
       <h1>Home</h1>
       <div className={styles.block}>
-        <p className={styles.red}>Welcome to my pretty basic boilerplate! It's goal is to show off how to use Webpack 3, React, CSS Modules and SASS together.</p>
+        <p className={styles.red}>Welcome to a basic boilerplate. It's goal is to show off how to use Webpack 3, React, CSS Modules and SASS together.</p>
+        <blockquote>npm install</blockquote>
+        <blockquote>npm start</blockquote>
       </div>
     </article>
   </div>
@@ -18,4 +20,3 @@ const Home = () => (
 render(
   <Home />,
   document.getElementById("main")
-);
